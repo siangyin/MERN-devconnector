50da087d2276136b5cb2008ece494b420c3841ca
Lee Siang Yin <siangyin916@gmail.com>
2022-01-04 16:43:44 +0800

    🏗 starter files setup, connectDB, basic routes testing 🆗

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/connectDB");
+
+//connect DB
+connectDB();
+
+app.get("/", (req, res) => {
+	res.send(`API running`);
+});
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+app.listen(PORT, () => {
+	console.log(`server on ${PORT}`);
+});
